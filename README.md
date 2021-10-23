a7ac9a467fd5ff1a9a5b3369de9f49c47ad63823
FX 2301 <realfx2301@gmail.com>
2021-10-23 02:48:25 -0700

    Add README.

--- README.md
+++ README.md
@@ -1,9 +1,97 @@
-# Running
+# Setup
 
-```bash
+```
 sudo apt install libpcap-dev
+git clone ...
+cd nmap_detect
+go build nmap_detect.go
+sudo setcap cap_net_raw,cap_net_admin=eip ./nmap_detect
 ```
 
-```bash
-go run .
+# Examples
+
+Begin detection:
+```
+./nmap_detect -i lo
+Interface lo has IP 127.0.0.1
+Interface lo has IP ::1
+Starting to read packets
+```
+
+## IPv4
+
+Port scan:
+```
+sudo nmap -sV --top-ports 10 127.0.0.1
+Starting Nmap 7.91 ( https://nmap.org ) at 2021-10-23 02:25 PDT
+Nmap scan report for localhost (127.0.0.1)
+Host is up (0.000013s latency).
+
+PORT     STATE  SERVICE       VERSION
+21/tcp   closed ftp
+22/tcp   closed ssh
+23/tcp   closed telnet
+25/tcp   closed smtp
+80/tcp   closed http
+110/tcp  closed pop3
+139/tcp  closed netbios-ssn
+443/tcp  closed https
+445/tcp  closed microsoft-ds
+3389/tcp closed ms-wbt-server
+
+Service detection performed. Please report any incorrect results at https://nmap.org/submit/ .
+Nmap done: 1 IP address (1 host up) scanned in 0.50 seconds
+```
+
+Observe detections:
+```
+SYN to 127.0.0.1:445 from 127.0.0.1
+SYN to 127.0.0.1:25 from 127.0.0.1
+SYN to 127.0.0.1:139 from 127.0.0.1
+SYN to 127.0.0.1:80 from 127.0.0.1
+SYN to 127.0.0.1:21 from 127.0.0.1
+SYN to 127.0.0.1:22 from 127.0.0.1
+SYN to 127.0.0.1:23 from 127.0.0.1
+SYN to 127.0.0.1:110 from 127.0.0.1
+SYN to 127.0.0.1:3389 from 127.0.0.1
+SYN to 127.0.0.1:443 from 127.0.0.1
+```
+
+## IPv6
+
+Port scan:
+```
+sudo nmap -sV --top-ports 10 -6 ::1
+Starting Nmap 7.91 ( https://nmap.org ) at 2021-10-23 02:25 PDT
+Nmap scan report for localhost (::1)
+Host is up (0.000017s latency).
+
+PORT     STATE  SERVICE       VERSION
+21/tcp   closed ftp
+22/tcp   closed ssh
+23/tcp   closed telnet
+25/tcp   closed smtp
+80/tcp   closed http
+110/tcp  closed pop3
+139/tcp  closed netbios-ssn
+443/tcp  closed https
+445/tcp  closed microsoft-ds
+3389/tcp closed ms-wbt-server
+
+Service detection performed. Please report any incorrect results at https://nmap.org/submit/ .
+Nmap done: 1 IP address (1 host up) scanned in 0.51 seconds
+```
+
+Observe detections:
+```
+SYN to ::1:139 from ::1
+SYN to ::1:443 from ::1
+SYN to ::1:80 from ::1
+SYN to ::1:110 from ::1
+SYN to ::1:3389 from ::1
+SYN to ::1:22 from ::1
+SYN to ::1:21 from ::1
+SYN to ::1:23 from ::1
+SYN to ::1:25 from ::1
+SYN to ::1:445 from ::1
 ```
---
+++ nmap_detect.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/google/gopacket/examples/util"
+	"github.com/google/gopacket/pcap"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers" // pulls in all layers decoders
+)
+
+var iface = flag.String("i", "eth0", "Interface to read packets from")
+
+func main() {
+	defer util.Run()()
+	var handle *pcap.Handle
+	var err error
+
+	var ief *net.Interface
+	if ief, err = net.InterfaceByName(*iface); err != nil {
+		log.Fatalf("could not get interface: %v", err)
+	}
+	var addrs []net.Addr
+	if addrs, err = ief.Addrs(); err != nil { // get addresses
+		log.Fatalf("could not get interface addresses: %v", err)
+	}
+	var ips []net.IP = make([]net.IP, len(addrs))
+	for i, addr := range addrs {
+		ips[i] = addr.(*net.IPNet).IP
+		fmt.Printf("Interface %s has IP %s\n", *iface, ips[i])
+	}
+
+	// This is a little complicated because we want to allow all possible options
+	// for creating the packet capture handle... instead of all this you can
+	// just call pcap.OpenLive if you want a simple handle.
+	inactive, err := pcap.NewInactiveHandle(*iface)
+	if err != nil {
+		log.Fatalf("could not create: %v", err)
+	}
+	defer inactive.CleanUp()
+	if err = inactive.SetSnapLen(65536); err != nil {
+		log.Fatalf("could not set snap length: %v", err)
+	} else if err = inactive.SetPromisc(true); err != nil {
+		log.Fatalf("could not set promisc mode: %v", err)
+	} else if err = inactive.SetTimeout(time.Second); err != nil {
+		log.Fatalf("could not set timeout: %v", err)
+	}
+	if handle, err = inactive.Activate(); err != nil {
+		log.Fatal("PCAP Activate error:", err)
+	}
+	defer handle.Close()
+
+	if len(flag.Args()) > 0 {
+		bpffilter := strings.Join(flag.Args(), " ")
+		fmt.Fprintf(os.Stderr, "Using BPF filter %q\n", bpffilter)
+		if err = handle.SetBPFFilter(bpffilter); err != nil {
+			log.Fatal("BPF filter error:", err)
+		}
+	}
+	Run(handle, ips)
+}
+
+func Run(src gopacket.PacketDataSource, ips []net.IP) {
+	if !flag.Parsed() {
+		log.Fatalln("Run called without flags.Parse() being called")
+	}
+	var dec gopacket.Decoder
+	var ok bool
+	if dec, ok = gopacket.DecodersByLayerName["Ethernet"]; !ok {
+		log.Fatalln("No decoder named", "Ethernet")
+	}
+	source := gopacket.NewPacketSource(src, dec)
+	source.Lazy = false
+	source.NoCopy = true
+	source.DecodeStreamsAsDatagrams = true
+	fmt.Fprintln(os.Stderr, "Starting to read packets")
+
+	for packet := range source.Packets() {
+		var transportLayer = packet.TransportLayer()
+		var networkLayer = packet.NetworkLayer()
+		if transportLayer != nil && transportLayer.LayerType() == layers.LayerTypeTCP {
+			var tcpLayer = transportLayer.(*layers.TCP)
+			var destIP net.IP
+			var sourceIP net.IP
+			if networkLayer.LayerType() == layers.LayerTypeIPv6 {
+				destIP = networkLayer.(*layers.IPv6).DstIP
+				sourceIP = networkLayer.(*layers.IPv6).SrcIP
+			} else {
+				destIP = networkLayer.(*layers.IPv4).DstIP
+				sourceIP = networkLayer.(*layers.IPv4).SrcIP
+			}
+			var inbound bool = false
+			for _, ip := range ips {
+				if net.IP.Equal(ip, destIP) {
+					inbound = true
+					break
+				}
+			}
+			if inbound {
+				if tcpLayer.SYN == true {
+					fmt.Printf("SYN to %s:%d from %s\n", destIP, tcpLayer.DstPort, sourceIP)
+				}
+			}
+		}
+	}
+}
